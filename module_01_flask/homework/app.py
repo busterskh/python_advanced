1160fc8936fd0c9f68fbe672260c62ff25cf65c9
Vlad Shevchenko <busterskh@gmail.com>
2022-07-04 12:28:51 +0300

    Done task in module 1

--- module_01_flask/homework/app.py
+++ module_01_flask/homework/app.py
@@ -1,34 +1,47 @@
 import datetime
 from flask import Flask
+import random
 
 app = Flask(__name__)
+current_time = datetime.datetime.now()
 
 
 @app.route('/hello_world')
-def test_function():
-   pass
+def hello_world():
+    return 'Привет, мир!'
 
 
 @app.route('/cars')
-def test_function():
-   pass
+def cars():
+    return 'Chevrolet, Renault, Ford, Lada'
 
 
 @app.route('/cats')
-def test_function():
-   pass
+def cats():
+    cats_list = ['Корниш рекс',
+                 'Русская голубая',
+                 'Шотландская вислоухая',
+                 'Мэйн-Кун',
+                 'Манчкин'
+                 ]
+    return random.choice(cats_list)
 
 
 @app.route('/get_time/now')
-def test_function():
-   pass
+def curr_time():
+    global current_time
+    return f'Точное время {current_time}'
 
 
 @app.route('/get_time/future')
-def test_function():
-   pass
+def time_after_hour():
+    global current_time
+    current_time_after_hour = current_time + datetime.timedelta(hours=+1)
+    return f'Точное время через час будет {current_time_after_hour}'
 
 
 @app.route('/get_random_word')
-def test_function():
-   pass
+def war_and_peace():
+    with open('war_and_peace.txt', 'r', encoding='utf-8') as file:
+        text = file.read()
+        return random.choice(text.split())
--- module_01_flask/materials/first_application/app.py
+++ module_01_flask/materials/first_application/app.py
@@ -2,8 +2,23 @@ import datetime
 from flask import Flask
 
 app = Flask(__name__)
+counter = 0
+
 
 @app.route('/test')
 def test_function():
    return 'Это тестовая страничка, ответ сгенерирован в %s' % \
                      datetime.datetime.now().utcnow()
+
+
+@app.route('/hello/world')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/counter')
+def counter_func():
+    global counter
+    counter += 1
+    return f'Страничка открывалась {counter} раз'
+
